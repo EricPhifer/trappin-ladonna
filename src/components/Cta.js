06733f1fe4cec785e6239d7736ad2fb25bed58d6
Eric Phifer <ericoncode@gmail.com>
2023-11-23 06:02:56 -0700

    update hero styling and arrangement, responsiveness verified

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -4,12 +4,12 @@ import { PiCaretRightBold } from 'react-icons/pi'
 import styled from 'styled-components'
 
 const Buttonesque = styled(Link)`
-  width: 24rem;
+  width: 22rem;
   margin-top: 4rem;
   background-color: var(--ctaback);
   color: var(--ctafore);
   border: 0.2rem solid var(--ctafore);
-  padding: 1.5rem 3rem;
+  padding: 1rem 2rem;
   font-size: 2.4rem;
   border-radius: 1rem;
   display: flex;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,23 +13,93 @@ const Header = styled.header`
   align-items: center;
   z-index: 0;
   // optional
-  background-image: linear-gradient(
-    to bottom,
-    var(--white),
-    orange,
-    var(--black)
+  background: linear-gradient(
+    0deg,
+    var(--black) 0%,
+    rgb(255, 127, 56) 31.51%,
+    rgb(255, 196, 108) 66.75%,
+    var(--white) 100%
   );
   img {
-    max-width: 60rem;
+    max-width: 100%;
     max-height: 70rem;
     position: absolute;
-    @media only screen and (max-height: 400px) {
-      top: -5rem;
+    top: 15%;
+    @media only screen and (max-width: 750px) {
+      max-width: 100%;
+      max-height: 100%;
+      top: 0;
     }
+    // Landscape View
+    @media only screen and (max-height: 500px) {
+      max-width: 100%;
+      max-height: 100%;
+      top: 0;
+    }
+  }
+
+  @media only screen and (max-width: 565px) {
+    height: calc(100vh - 26rem);
+    top: 0;
   }
   // Landscape view
   @media only screen and (max-height: 600px) {
-    height: calc(100% + 10rem);
+    height: calc(100vh - 13rem);
+  }
+`
+
+const Alignment = styled.div`
+  max-width: 108rem;
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  position: relative;
+`
+
+const Right = styled.div`
+  max-width: 50%;
+  width: 50%;
+  height: 100%;
+  margin: 0 auto;
+  position: absolute;
+  right: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media only screen and (max-width: 750px) {
+    max-width: 100%;
+    max-height: 50%;
+    width: 100%;
+    top: 0;
+  }
+  // Landscape View
+  @media only screen and (max-height: 420px) {
+    max-height: 100%;
+    max-width: 50%;
+  }
+`
+
+const Left = styled.div`
+  max-width: 50%;
+  width: 50%;
+  height: 100%;
+  margin: 0 auto;
+  position: absolute;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column nowrap;
+  @media only screen and (max-width: 750px) {
+    max-width: 100%;
+    width: 100%;
+    max-height: 50%;
+    bottom: 0;
+  }
+  // Landscape View
+  @media only screen and (max-height: 420px) {
+    max-height: 100%;
+    max-width: 50%;
   }
 `
 
@@ -44,30 +114,42 @@ const Container = styled.div`
   color: var(--white);
   text-align: center;
   z-index: 10;
+  .cta {
+    align-self: end;
+    @media only screen and (max-width: 750px) {
+      align-self: center;
+      margin: 0;
+    }
+  }
+
   // Mobile view
   @media only screen and (max-width: 750px) {
-    height: 90%;
+    height: 100%;
   }
-  @media only screen and (max-height: 500px) {
-    height: calc(100% - 15rem);
-  }
+  // @media only screen and (max-height: 500px) {
+  //   height: calc(100% - 15rem);
+  // }
+`
+
+const TitleContainer = styled.div`
+  max-height: 10rem;
+  display: flex;
+  align-items: center;
+  position: relative;
+  margin: 2rem 0;
 `
 
 const Title = styled.h1`
-  font-size: 8rem;
-  line-height: 1;
+  width: 100%;
+  font-size: 4.5rem;
+  text-transform: uppercase;
+  line-height: 1.5;
+  letter-spacing: 1.15rem;
   text-shadow: 0.1rem 0 0 var(--gray), -0.1rem 0 0 var(--gray),
     0 0.1rem 0 var(--gray), 0 -0.1rem 0 var(--gray);
+
   // Mobile view
-  @media only screen and (max-width: 750px) {
-    font-size: 6rem;
-  }
-  @media only screen and (max-width: 615px) {
-    font-size: 5rem;
-  }
-  @media only screen and (max-width: 500px) {
-    font-size: 3rem;
-  }
+
   // Landscape view
   @media only screen and (max-height: 600px) {
     font-size: 3.75rem;
@@ -76,26 +158,21 @@ const Title = styled.h1`
 `
 
 const Motto = styled.h2`
+  width: 100%;
   margin: 0;
-  font-size: 3.5rem;
+  font-size: 2rem;
+  color: gold;
+  position: absolute;
+  text-align: center;
+  top: 40%;
+  padding-right: 2rem;
   text-shadow: 0.1rem 0 0 var(--gray), -0.1rem 0 0 var(--gray),
     0 0.1rem 0 var(--gray), 0 -0.1rem 0 var(--gray);
-  .address {
-    font-size: 3rem;
-    @media only screen and (max-width: 615px) {
-      font-size: 2.5rem;
-    }
-    @media only screen and (max-width: 500px) {
-      font-size: 2rem;
-    }
-  }
+
   // Mobile view
-  @media only screen and (max-width: 615px) {
-    font-size: 3.5rem;
+  @media only screen and (max-width: 750px) {
   }
-  @media only screen and (max-width: 500px) {
-    font-size: 2.15rem;
-  }
+
   // Landscape view
   @media only screen and (max-height: 600px) {
     font-size: 2rem;
@@ -127,24 +204,32 @@ export default function StoryHero() {
     <>
       {nodes.map(node => (
         <Header key={node.id}>
-          {node.image ? (
-            <SanityImage
-              {...node.image}
-              alt={node.alt}
-              style={{
-                backgroundImage: 'fixed',
-                objectFit: 'cover',
-                auto: 'format',
-              }}
-            />
-          ) : (
-            <div />
-          )}
-          <Container>
-            <Title>{node.title}</Title>
-            <Motto>{node.tagline}</Motto>
-            <Cta />
-          </Container>
+          <Alignment>
+            <Right>
+              {node.image ? (
+                <SanityImage
+                  {...node.image}
+                  alt={node.alt}
+                  style={{
+                    backgroundImage: 'fixed',
+                    objectFit: 'cover',
+                    auto: 'format',
+                  }}
+                />
+              ) : (
+                <div />
+              )}
+            </Right>
+            <Left>
+              <Container>
+                <TitleContainer>
+                  <Title>{node.title}</Title>
+                  <Motto>{node.tagline}</Motto>
+                </TitleContainer>
+                <Cta />
+              </Container>
+            </Left>
+          </Alignment>
         </Header>
       ))}
     </>
